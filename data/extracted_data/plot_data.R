bd41ecc2970dd975490f4b3caaca5e7c85e3fd1f
cfree14 <cfree14@gmail.com>
2025-07-25 08:26:56 -0700

    extracted data work

--- data/extracted_data/plot_data.R
+++ data/extracted_data/plot_data.R
@@ -156,10 +156,10 @@ plot_data <- function(datafile, day=NA, y_title="Toxicity", legend_title="Treatm
     # Plot reference line
     geom_vline(xintercept=day, linetype="dashed", color="grey50") +
     # Plot data
-    geom_line(linetype="dotted") +
-    geom_point(size=2) +
+    geom_line(size=0.1) +
+    geom_point(size=1.2) +
     # Plot fits
-    geom_line(data=fits, size=1.5) +
+    geom_line(data=fits, size=1) +
     geom_text(data=coefs, mapping=aes(label=paste0("k = ", round(k, 5)), x=day2, y=y), 
               hjust=1, size=1.8, show.legend=F) +
     # Axes
@@ -215,9 +215,9 @@ plot_data(datafile="Kim_etal_2017_Fig1.xlsx",
 plot_data(datafile="Kim_etal_2018_Fig1.xlsx",
           day=7, y_title="Toxicity (mg/g)", legend_title="Exposure (mg/L)", title="Kim et al. (2018)")
 
-# # Lewis et al. (2022)
-# plot_data(datafile="Lewis_etal_2022_Fig3.xlsx",
-#           day=7, y_title="Toxicity (ug/kg)", legend_title="Treatment", title="Lewis et al. (2022)")
+# Lewis et al. (2022)
+plot_data(datafile="Lewis_etal_2022_Fig3.xlsx",
+          day=7, y_title="Toxicity (ug/kg)", legend_title="Treatment", title="Lewis et al. (2022)")
 
 # Gibble et al. (2016) -- this needs checking
 plot_data(datafile="Gibble_etal_2016_Fig1-5.xlsx",
@@ -227,34 +227,34 @@ plot_data(datafile="Gibble_etal_2016_Fig1-5.xlsx",
 plot_data(datafile="Min_etal_2018_Fig1.xlsx",
           day=7, y_title="Toxicity (ug/g)", legend_title="Exposure (ug/L)", title="Min et al. (2018)")
 
-# # Takata et al. (2008)
-# plot_data(datafile="Takata_etal_2008_Fig1.xlsx",
-#           day=0, y_title="Toxicity (MU/g)", legend_title="Year", title="Takata et al. (2008)")
-# 
-# # Lavaud et al. (2021) - problems here
-# plot_data(datafile="Lavaud_etal_2021_Fig2.xlsx",
-#           day=1, y_title="Toxicity (ug/100g)", title="Lavaud et al. (2021)")
-# 
-# # Yang et al. (2021)
-# plot_data(datafile="Yang_etal_2021_Fig1-3.xlsx",
-#           day=0, y_title="Toxicity (MU/100g)", legend_title="Food type", title="Yang et al. (2021)")
-# 
-# # Kwong et al. (2006)
-# plot_data(datafile="Kwong_etal_2006_Fig2.xlsx",
-#           day=6, y_title="Toxicity (ug/100g)", legend_title="Tissue", title="Kwong et al. (2006)")
-# 
-# # Martins et al. (2006)
-# plot_data(datafile="Martins_etal_2020_Fig1.xlsx",
-#           day=5, y_title="Toxicity (ug/kg)", legend_title="Toxin", title="Martins et al. (2020)")
-# 
-# # Sekiguchi et al. (2001)
-# plot_data(datafile="Sekiguchi_etal_2001_Fig2.xlsx",
-#           day=5, y_title="Toxicity (nmol/specimen)", legend_title="Toxin", title="Sekiguchi et al. (2001)")
-# 
-# # Chen et al. (2002)
-# plot_data(datafile="Chen_etal_2002_Fig1.xlsx",
-#           day=33, y_title="Toxicity (MU/g)",  title="Chen et al. (2002)")
-# 
+# Takata et al. (2008)
+plot_data(datafile="Takata_etal_2008_Fig1.xlsx",
+          day=0, y_title="Toxicity (MU/g)", legend_title="Year", title="Takata et al. (2008)")
+
+# Lavaud et al. (2021) - problems here
+plot_data(datafile="Lavaud_etal_2021_Fig2.xlsx",
+          day=8, y_title="Toxicity (ug/100g)", title="Lavaud et al. (2021)")
+
+# Yang et al. (2021)
+plot_data(datafile="Yang_etal_2021_Fig1-3.xlsx",
+          day=0, y_title="Toxicity (MU/100g)", legend_title="Food type", title="Yang et al. (2021)")
+
+# Kwong et al. (2006)
+plot_data(datafile="Kwong_etal_2006_Fig2.xlsx",
+          day=6, y_title="Toxicity (ug/100g)", legend_title="Tissue", title="Kwong et al. (2006)")
+
+# Martins et al. (2006)
+plot_data(datafile="Martins_etal_2020_Fig1.xlsx",
+          day=5, y_title="Toxicity (ug/kg)", legend_title="Toxin", title="Martins et al. (2020)")
+
+# Sekiguchi et al. (2001)
+plot_data(datafile="Sekiguchi_etal_2001_Fig2.xlsx",
+          day=8, y_title="Toxicity (nmol/specimen)", legend_title="Toxin", title="Sekiguchi et al. (2001)")
+
+# Chen et al. (2002)
+plot_data(datafile="Chen_etal_2002_Fig1.xlsx",
+          day=36, y_title="Toxicity (MU/g)",  title="Chen et al. (2002)")
+
 # # Houle et al. (2023) - lab
 # plot_data(datafile="Houle_etal_2023_Fig8.xlsx",
 #           day=0, y_title="Toxicity (ug /100g)", legend_title="Tissue", title="Houle et al. (2023)")
@@ -264,15 +264,15 @@ plot_data(datafile="Min_etal_2018_Fig1.xlsx",
 # #   mutate(date=lubridate::mdy(date),
 # #          day=difftime(date, min(date), units = "days") %>% as.numeric()) 
 # # plot_data(datafile=, day=0, y_title="Toxicity (ug/100g)", legend_title="Exposure (ug/L)", title="Houle et al. (2023)")
-# 
-# # Qiu et al. (2018)
-# plot_data(datafile="Qiu_etal_2018_Fig4.xlsx",
-#           day=5, y_title="Toxicity (umol/kg)", legend_title="Toxin", title="Qiu et al. (2018)")
-# 
-# # Svensson et al. (2003) - the data points aren't quite right - not connected right
-# plot_data(datafile="Svensson_etal_2003_Fig2.xlsx", 
-#           day=5, y_title="Toxicity (ug/g)", legend_title="Food type", title="Svensson et al. (2003)")
-# 
-# 
-# 
-# 
+
+# Qiu et al. (2018)
+plot_data(datafile="Qiu_etal_2018_Fig4.xlsx",
+          day=5, y_title="Toxicity (umol/kg)", legend_title="Toxin", title="Qiu et al. (2018)")
+
+# Svensson et al. (2003) - the data points aren't quite right - not connected right
+plot_data(datafile="Svensson_etal_2003_Fig2.xlsx",
+          day=2, y_title="Toxicity (ug/g)", legend_title="Food type", title="Svensson et al. (2003)")
+
+
+
+
